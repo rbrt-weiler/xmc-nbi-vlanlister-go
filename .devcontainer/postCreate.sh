0e5c1f2e1ad36200fc5adae5948e3e167e910d2e
Robert WEILER <rbrt-weiler@mailbox.org>
2020-04-19 09:03:03 +0200

    Updated dependencies.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,6 +2,7 @@
 
 go get -u golang.org/x/tools/cmd/goimports
 
+go get -u github.com/360EntSecGroup-Skylar/excelize
 go get -u github.com/joho/godotenv
 go get -u gitlab.com/rbrt-weiler/go-module-envordef
-go get -u gitlab.com/rbrt-weiler/go-module-xiqrestclient
+go get -u gitlab.com/rbrt-weiler/go-module-xmcnbiclient
