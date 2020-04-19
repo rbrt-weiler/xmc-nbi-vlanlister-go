e112058a85161c8c08680a46203a38741dc200b8
Robert WEILER <rbrt-weiler@mailbox.org>
2020-04-19 10:26:51 +0200

    VS Code Container upgraded. Now includes a set of Go tools.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-go get -u golang.org/x/tools/cmd/goimports
+# Usual VS Code suspects
+#go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/stamblerre/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/lint/golint
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
 
-go get -u github.com/360EntSecGroup-Skylar/excelize
-go get -u github.com/joho/godotenv
-go get -u gitlab.com/rbrt-weiler/go-module-envordef
-go get -u gitlab.com/rbrt-weiler/go-module-xmcnbiclient
+# Project-specific dependencies
+go get -u -v github.com/360EntSecGroup-Skylar/excelize
+go get -u -v github.com/joho/godotenv
+go get -u -v gitlab.com/rbrt-weiler/go-module-envordef
+go get -u -v gitlab.com/rbrt-weiler/go-module-xmcnbiclient
