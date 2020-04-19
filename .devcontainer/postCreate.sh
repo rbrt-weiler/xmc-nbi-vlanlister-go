6bdda3061cd9f5e99772c9cf3b77b4f34a3c571b
Robert WEILER <rbrt-weiler@mailbox.org>
2020-04-19 08:57:30 +0200

    Added VS Code Remote Container config.

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go get -u golang.org/x/tools/cmd/goimports
+
+go get -u github.com/joho/godotenv
+go get -u gitlab.com/rbrt-weiler/go-module-envordef
+go get -u gitlab.com/rbrt-weiler/go-module-xiqrestclient
