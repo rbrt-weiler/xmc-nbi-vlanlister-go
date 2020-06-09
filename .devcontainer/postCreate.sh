c6618df7d4c7c2b0fabda1ef786595bc02e13003
Robert WEILER <rbrt-weiler@mailbox.org>
2020-06-09 21:40:23 +0200

    Update DevContainer settings.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Usual VS Code suspects
-#go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/mdempsky/gocode
 go get -u -v github.com/stamblerre/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v golang.org/x/lint/golint
 go get -u -v github.com/ramya-rao-a/go-outline
 go get -u -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
-
-# Project-specific dependencies
-go get -u -v github.com/360EntSecGroup-Skylar/excelize
-go get -u -v github.com/joho/godotenv
-go get -u -v gitlab.com/rbrt-weiler/go-module-envordef
-go get -u -v gitlab.com/rbrt-weiler/go-module-xmcnbiclient
